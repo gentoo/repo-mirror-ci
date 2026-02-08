0f39f67f653baead7c02e15d79e63429f813ea32
Thomas Bracht Laumann Jespersen <t@laumann.xyz>
2026-02-08 07:43:16 +0100

    pull-request: add scanning of codeberg PRs
    Extend scan-pull-requests.py to scan the configured Codeberg repo for
    PRs and push them into the same queue. The PRs from Codeberg take
    priority over the GitHub ones.
    
    Based on the forge prefix we then select which remote to fetch PR
    changes from for testing.
    
    Signed-off-by: Thomas Bracht Laumann Jespersen <t@laumann.xyz>

--- pull-request/scan-pull-requests.py
+++ pull-request/scan-pull-requests.py
@@ -8,11 +8,98 @@ import errno
 import os
 import pickle
 import sys
+from datetime import datetime
 
 import github
+from codebergapi import CodebergAPI
 
 
-def scan_github(db: dict):
+def scan_codeberg(db: dict):
+    CODEBERG_USERNAME = os.environ["CODEBERG_USERNAME"]
+    CODEBERG_TOKEN_FILE = os.environ["CODEBERG_TOKEN_FILE"]
+    (owner, repo) = os.environ["CODEBERG_REPO"].split("/")
+
+    with open(CODEBERG_TOKEN_FILE) as f:
+        token = f.read().strip()
+
+    with CodebergAPI(owner, repo, token) as cb:
+        to_process = []
+        for pr in cb.pulls():
+            pr_key = f"codeberg/{pr['number']}"
+            sha = pr["head"]["sha"]
+
+            # skip PRs marked noci
+            if any(x["name"] == "noci" for x in pr["labels"]):
+                print(f"{pr_key}: noci", file=sys.stderr)
+
+                # if it made it to the cache, we probably need to wipe
+                # pending status
+                if pr_key in db:
+                    statuses = cb.commit_statuses(sha)
+                    for status in statuses:
+                        # skip foreign statuses
+                        if status["creator"]["login"] != CODEBERG_USERNAME:
+                            continue
+                        # if it's pending, mark it done
+                        if status["status"] == "pending":
+                            cb.commit_set_status(
+                                sha,
+                                "success",
+                                description="Checks skipped due to [noci] label",
+                                context="gentoo-ci",
+                            )
+                        break
+                    del db[pr_key]
+
+                continue
+
+            # if it's not cached, get its status
+            if pr_key not in db:
+                print(f"{pr_key}: updating status ...", file=sys.stderr)
+                statuses = cb.commit_statuses(sha)
+                for status in statuses:
+                    # skip foreign statuses
+                    if status["creator"]["login"] != CODEBERG_USERNAME:
+                        continue
+                    # if it's not pending, mark it done
+                    if status["status"] == "pending":
+                        db[pr_key] = ""
+                        print(f"{pr_key}: found pending", file=sys.stderr)
+                    else:
+                        db[pr_key] = sha
+                        print(f"{pr_key}: at {sha}", file=sys.stderr)
+                    break
+                else:
+                    db[pr_key] = ""
+                    print(f"{pr_key}: unprocessed", file=sys.stderr)
+
+            if db.get(pr_key, "") != sha:
+                to_process.append(pr)
+
+        to_process = sorted(
+            to_process,
+            key=lambda x: (
+                not any(x["name"] == "priority-ci" for x in pr["labels"]),
+                datetime.fromisoformat(x["updated_at"]),
+            ),
+        )
+        queue = []
+        for i, pr in enumerate(to_process):
+            pr_key = f"codeberg/{pr['number']}"
+            sha = pr["head"]["sha"]
+            if i == 0:
+                desc = "QA checks in progress..."
+                db[pr_key] = sha
+            else:
+                desc = "QA checks pending. Currently {}. in queue.".format(i)
+            cb.commit_set_status(sha, "pending", description=desc, context="gentoo-ci")
+
+            print(f"{pr_key}: {db.get(pr_key, '') or '(none)'} -> {sha}", file=sys.stderr)
+            queue.append(pr_key)
+        return queue
+
+
+def scan_github(db: dict, queue_len: int):
     """
     Given a db of knowns PRs, inspect open PRs, update commit
     statuses, and update the db accordingly. Return a list of
@@ -89,6 +176,7 @@ def scan_github(db: dict):
             x.updated_at,
         ),
     )
+    queue = []
     for i, pr in enumerate(to_process):
         pr_key = f"github/{pr.number}"
         db_key = pr.number if pr.number in db else pr_key
@@ -97,14 +185,15 @@ def scan_github(db: dict):
             desc = "QA checks in progress..."
             db[db_key] = commit.sha
         else:
-            desc = f"QA checks pending. Currently {i}. in queue."
+            desc = f"QA checks pending. Currently {i+queue_len}. in queue."
         commit.create_status(context="gentoo-ci", state="pending", description=desc)
 
         print(
             f"{pr_key}: {db.get(db_key, '') or '(none)'} -> {pr.head.sha}", file=sys.stderr
         )
+        queue.append(pr_key)
 
-    return to_process
+    return queue
 
 
 def main():
@@ -118,14 +207,15 @@ def main():
         if e.errno != errno.ENOENT:
             raise
 
-    to_process = scan_github(db)
+    queue = scan_codeberg(db)
+    queue.extend(scan_github(db, len(queue)))
 
     with open(PULL_REQUEST_DB + ".tmp", "wb") as f:
         pickle.dump(db, f)
     os.rename(PULL_REQUEST_DB + ".tmp", PULL_REQUEST_DB)
 
-    if to_process:
-        print(f"github/{to_process[0].number}")
+    if queue:
+        print(queue[0])
 
     return 0
 
