gentoo/repo-mirror-ci

commit 2528895924727cf7bd491c4f21c219e7033df02f

gentoo-ci.bash: run pkgcheck as a separate user
Like b0441fb47885bb08a58adad53f40d228f1057b81 and 794969f71431ad9b871b267f7775945f44f08f46
(latter for non-PRs).

A followup will add setpriv(1) use like in the pmaint side, see
292d95b9b9d363b97f9aeb9a30047564c7bb7b7d and followups there.

Signed-off-by: Sam James <sam@gentoo.org>

gentoo-ci/gentoo-ci.bash
@@ -26,11 +26,16 @@ if [[ ${PREV_COMMIT} != ${CURRENT_COMMIT} ]]; then
 	fi
 
 	export CONFIG_DIR=${CONFIG_ROOT_GENTOO_CI}/etc/portage
-	( cd -- "${MIRROR_DIR}"/gentoo &&
+
+	# TODO: setpriv
+	cd -- "${MIRROR_DIR}"/gentoo
+	sudo -u "${WORKER_USER}" \
+		bwrap --bind / / --dev /dev --proc /proc --unshare-all \
+		--uid $(id -u "${WORKER_USER}") --gid $(id -g "${WORKER_USER}") \
 		time timeout -k 30s "${CI_TIMEOUT}" pkgcheck --config "${CONFIG_DIR}" scan \
-			--reporter XmlReporter ${PKGCHECK_OPTIONS}
-	) | xsltproc "${SCRIPT_DIR}"/sort-output.xsl - > output.xml
-	# ^^ Sort XML for better Git delta compression
+		--reporter XmlReporter ${PKGCHECK_OPTIONS} > output.xml.tmp
+	# Sort XML for better Git delta compression
+	cat output.xml.tmp | xsltproc "${SCRIPT_DIR}"/sort-output.xsl - > output.xml
 
 	"${PKGCHECK_RESULT_PARSER_GIT}"/pkgcheck2borked.py \
 		-x "${PKGCHECK_RESULT_PARSER_GIT}"/excludes.json \

commit cf660dd5a71906c081fb4f898fe21c83f8902cb0

gentoo-ci.bash: fix working directory for output.xml
output.xml would end up committed otherwise.

Fixes: 2528895924727cf7bd491c4f21c219e7033df02f
Signed-off-by: Sam James <sam@gentoo.org>

gentoo-ci/gentoo-ci.bash
@@ -173,7 +173,7 @@ if [[ ${PREV_COMMIT} != ${CURRENT_COMMIT} ]]; then
 
 	create_pkgcheck_setpriv_wrapper
 
-	cd -- "${MIRROR_DIR}"/gentoo
+	pushd -- "${MIRROR_DIR}"/gentoo >/dev/null
 	sudo -u "${WORKER_USER}" \
 		bwrap --bind / / --dev /dev --proc /proc --unshare-all \
 		--uid $(id -u "${WORKER_USER}") --gid $(id -g "${WORKER_USER}") \
@@ -184,8 +184,10 @@ if [[ ${PREV_COMMIT} != ${CURRENT_COMMIT} ]]; then
 		"${MIRROR_DIR}/${name}" \
 		pkgcheck --config "${CONFIG_DIR}" scan \
 		--reporter XmlReporter ${PKGCHECK_OPTIONS} > output.xml.tmp
+	popd >/dev/null
 	# Sort XML for better Git delta compression
-	cat output.xml.tmp | xsltproc "${SCRIPT_DIR}"/sort-output.xsl - > output.xml
+	cat "${MIRROR_DIR}"/output.xml.tmp | xsltproc "${SCRIPT_DIR}"/sort-output.xsl - > output.xml
+	rm "${MIRROR_DIR}"/gentoo/output.xml.tmp
 
 	"${PKGCHECK_RESULT_PARSER_GIT}"/pkgcheck2borked.py \
 		-x "${PKGCHECK_RESULT_PARSER_GIT}"/excludes.json \