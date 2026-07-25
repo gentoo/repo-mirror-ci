c74a56abaaca2ed0fbc4209c8ab95de53a7b7c4c
Sam James <sam@gentoo.org>
2026-07-25 03:28:39 +0100

    repos.bash: apply setpriv wrapper to pmaint sync too
    Some code duplication for now.
    
    Signed-off-by: Sam James <sam@gentoo.org>

--- repos/repos.bash
+++ repos/repos.bash
@@ -9,15 +9,20 @@ export TZ=UTC
 date=$(date -u "+%Y-%m-%dT%H:%M:%SZ")
 
 # Wrapper around setpriv(1) for landlock. We want to limit what a compromised
-# pmaint regen process can do (as it sources untrusted ebuilds), including
-# not being able to tamper with other repositories being processed.
-create_pmaint_setpriv_wrapper() {
-	cat <<-EOF > /var/lib/repo-mirror-ci/pmaint-wrapper
+# pmaint sync process can do, including not being able to tamper with other
+# repositories being processed.
+#
+# TODO: deduplicate with create_pmaint_setpriv_wrapper, most of it is
+# shared.
+create_pmaint_sync_setpriv_wrapper() {
+	cat <<-EOF > /var/lib/repo-mirror-ci/pmaint-sync-wrapper
 	#!/bin/bash
 	set -x
 
 	portage_dir=\$1
-	repo_dir=\$2
+	repos_dir=\$2
+	repo_dir=\$3
+	shift
 	shift
 	shift
 
@@ -63,8 +68,155 @@ create_pmaint_setpriv_wrapper() {
 
 		# Any repository may need to read any other repository because
 		# of repo masters.
-		--landlock-rule path-beneath:read-dir:${REPOS_DIR}
-		--landlock-rule path-beneath:read-file:${REPOS_DIR}
+		--landlock-rule path-beneath:read-dir:\${repos_dir}
+		--landlock-rule path-beneath:read-file:\${repos_dir}
+
+		# Only allow writing to the specific repo we're operating on.
+		--landlock-rule path-beneath:write-file:\${repo_dir}/metadata
+		--landlock-rule path-beneath:write-file:\${repo_dir}/profiles
+		--landlock-rule path-beneath:make-dir:\${repo_dir}/metadata
+		--landlock-rule path-beneath:make-reg:\${repo_dir}
+		--landlock-rule path-beneath:remove-file:\${repo_dir}/metadata
+		--landlock-rule path-beneath:remove-file:\${repo_dir}/profiles
+	)
+
+	# Needed for Python to be able to find libb2 for hashlib
+	for file in /etc/ld.so.cache ; do
+		setpriv_args+=(
+			--landlock-rule path-beneath:read-file:\${file}
+		)
+	done
+	# Needed to call pmaint or called by pmaint
+	for file in /usr/bin/pmaint /usr/bin/python3.?? /usr/bin/python-exec2c /bin/bash \
+			/usr/bin/sandbox /bin/stty /usr/bin/env \
+			/usr/bin/readlink /usr/bin/sort ; do
+		setpriv_args+=(
+			--landlock-rule path-beneath:read-file:\${file}
+			--landlock-rule path-beneath:execute:\${file}
+		)
+	done
+	for dir in /usr/lib/python-exec /usr/lib64/python-exec ; do
+		setpriv_args+=(
+			--landlock-rule path-beneath:read-dir:\${dir}
+			--landlock-rule path-beneath:read-file:\${dir}
+			--landlock-rule path-beneath:execute:\${dir}
+		)
+	done
+	# Not just for Python itself but also the loader..
+	for dir in /usr/lib64 /lib64 /usr/lib/pkgcore ; do
+		setpriv_args+=(
+			--landlock-rule path-beneath:read-dir:\${dir}
+			--landlock-rule path-beneath:read-file:\${dir}
+			--landlock-rule path-beneath:execute:\${dir}
+		)
+	done
+	# site-packages
+	for dir in /usr/lib/python3.?? /etc/python-exec ; do
+		setpriv_args+=(
+			--landlock-rule path-beneath:read-dir:\${dir}
+			--landlock-rule path-beneath:read-file:\${dir}
+		)
+	done
+	# portage+pkgcore config
+	for dir in /usr/share/portage /usr/share/pkgcore ; do
+		setpriv_args+=(
+			--landlock-rule path-beneath:read-dir:\${dir}
+			--landlock-rule path-beneath:read-file:\${dir}
+		)
+	done
+
+	#
+	# same as create_pmaint_setpriv_wrapper above except for wrapper name
+	#
+
+	# Sync methods
+	for bin in /usr/bin/git /usr/libexec/git-core ; do
+		setpriv_args+=(
+			--landlock-rule path-beneath:read-file:\${bin}
+			--landlock-rule path-beneath:execute:\${bin}
+		)
+	done
+	setpriv_args+=(
+		--landlock-rule path-beneath:read-file:/etc/resolv.conf
+		--landlock-rule path-beneath:read-file:/etc/nsswitch.conf
+		--landlock-rule path-beneath:read-file:/etc/ssl/certs/ca-certificates.crt
+
+		# Only allow writing to the specific repo we're operating on.
+		--landlock-rule path-beneath:read-dir:\${repo_dir}
+		--landlock-rule path-beneath:read-file:\${repo_dir}
+		--landlock-rule path-beneath:write-file:\${repo_dir}
+		--landlock-rule path-beneath:make-dir:\${repo_dir}
+		--landlock-rule path-beneath:make-reg:\${repo_dir}
+		--landlock-rule path-beneath:remove-dir:\${repo_dir}
+		--landlock-rule path-beneath:remove-file:\${repo_dir}
+		# git uses truncate for some files in .git
+		--landlock-rule path-beneath:truncate:\${repo_dir}
+	)
+
+	exec setpriv "\${setpriv_args[@]}" -- "\$@"
+	EOF
+	chmod +x /var/lib/repo-mirror-ci/pmaint-sync-wrapper
+}
+
+# Wrapper around setpriv(1) for landlock. We want to limit what a compromised
+# pmaint regen process can do (as it sources untrusted ebuilds), including
+# not being able to tamper with other repositories being processed.
+create_pmaint_setpriv_wrapper() {
+	cat <<-EOF > /var/lib/repo-mirror-ci/pmaint-wrapper
+	#!/bin/bash
+	set -x
+
+	portage_dir=\$1
+	repos_dir=\$2
+	repo_dir=\$3
+	shift
+	shift
+	shift
+
+	setpriv_args=(
+		--landlock-access fs
+
+		--landlock-rule path-beneath:read-file:/dev/null
+		--landlock-rule path-beneath:write-file:/dev/null
+
+		# Try to let pmaint emit errors when run under cron
+		--landlock-rule path-beneath:read-file:/dev/stdout
+		--landlock-rule path-beneath:read-file:/dev/stderr
+		--landlock-rule path-beneath:write-file:/dev/stdout
+		--landlock-rule path-beneath:write-file:/dev/stderr
+		--landlock-rule path-beneath:ioctl-dev:/dev/stdout
+		--landlock-rule path-beneath:ioctl-dev:/dev/stderr
+		--landlock-rule path-beneath:ioctl-dev:/dev/tty
+		--landlock-rule path-beneath:read-file:/dev/pts
+		--landlock-rule path-beneath:read-dir:/dev/pts
+		--landlock-rule path-beneath:write-file:/dev/pts
+		--landlock-rule path-beneath:ioctl-dev:/dev/pts
+
+		--landlock-rule path-beneath:read-file:/dev/tty
+		--landlock-rule path-beneath:write-file:/dev/tty
+
+		--landlock-rule path-beneath:write-file:/tmp
+
+		# sandbox.log
+		--landlock-rule path-beneath:make-reg:/tmp
+		--landlock-rule path-beneath:remove-file:/tmp
+
+		--landlock-rule path-beneath:read-dir:/etc/sandbox.d
+		--landlock-rule path-beneath:read-file:/etc/sandbox.d
+		--landlock-rule path-beneath:read-file:/etc/sandbox.conf
+		--landlock-rule path-beneath:read-file:/usr/share/sandbox/sandbox.bashrc
+
+		# Needed for make.profile symlink
+		--landlock-rule path-beneath:read-dir:/var/db/repos/gentoo
+		--landlock-rule path-beneath:read-file:/var/db/repos/gentoo
+
+		--landlock-rule path-beneath:read-dir:\${portage_dir}
+		--landlock-rule path-beneath:read-file:\${portage_dir}
+
+		# Any repository may need to read any other repository because
+		# of repo masters.
+		--landlock-rule path-beneath:read-dir:\${repos_dir}
+		--landlock-rule path-beneath:read-file:\${repos_dir}
 
 		# Only allow writing to the specific repo we're operating on.
 		--landlock-rule path-beneath:write-file:\${repo_dir}/metadata
@@ -168,12 +320,18 @@ do
 	fi
 done
 
+create_pmaint_sync_setpriv_wrapper
+create_pmaint_setpriv_wrapper
+
 # sync all repos
 for r in ${REPOS}; do
 	name=${r%%:*}
 
-	# TODO: setpriv
-	pmaint --config "${CONFIG_ROOT_SYNC}/etc/portage" sync "${name}"
+	/var/lib/repo-mirror-ci/pmaint-sync-wrapper \
+		"${CONFIG_ROOT_SYNC}/etc/portage" \
+		"${SYNC_DIR}" \
+		"${SYNC_DIR}/${name}" \
+		pmaint --config "${CONFIG_ROOT_SYNC}/etc/portage" sync "${name}"
 done
 
 # check signed repos
@@ -202,8 +360,6 @@ setfacl -d -R -m u:${USER}:rwx "${REPOS_DIR}" ||:
 # entries.
 setfacl -d -R -m g:${USER}:rwx "${REPOS_DIR}" ||:
 
-create_pmaint_setpriv_wrapper
-
 # prepare mirrors
 for r in ${REPOS}; do
 	name=${r%%:*}
@@ -212,7 +368,8 @@ for r in ${REPOS}; do
 	sudo -u "${WORKER_USER}" \
 		bwrap --bind / / --dev /dev --proc /proc --unshare-all \
 		--uid $(id -u "${WORKER_USER}") --gid $(id -g "${WORKER_USER}") \
-		/var/lib/repo-mirror-ci/pmaint-wrapper "${CONFIG_ROOT}/etc/portage" "${REPOS_DIR}/${name}" \
+		/var/lib/repo-mirror-ci/pmaint-wrapper \
+		"${CONFIG_ROOT}/etc/portage" "${REPOS_DIR}" "${REPOS_DIR}/${name}" \
 		pmaint --config "${CONFIG_ROOT}/etc/portage" regen \
 		--use-local-desc --pkg-desc-index -t "$(nproc)" "${name}"
 
