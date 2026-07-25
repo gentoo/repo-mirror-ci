50544d20195bfa0403c0ae09646a94499d3abe6f
Sam James <sam@gentoo.org>
2026-07-25 09:01:19 +0100

    gentoo-ci.bash: use util-linux's setpriv(1) for landlock on pkgcheck scan
    As promised in 2528895924727cf7bd491c4f21c219e7033df02f.
    
    Signed-off-by: Sam James <sam@gentoo.org>

--- gentoo-ci/gentoo-ci.bash
+++ gentoo-ci/gentoo-ci.bash
@@ -5,6 +5,150 @@ set -e -x
 # SANITY!
 export TZ=UTC
 
+# Wrapper around setpriv(1) for landlock. We want to limit what a compromised
+# pkgcheck process can do (as it sources untrusted ebuilds)
+create_pkgcheck_setpriv_wrapper() {
+	cat <<-EOF > /var/lib/repo-mirror-ci/pkgcheck-wrapper
+	#!/bin/bash
+	set -x
+
+	portage_dir=\$1
+	repos_dir=\$2
+	repo_dir=\$3
+	shift
+	shift
+	shift
+
+	setpriv_args=(
+		--landlock-access fs
+
+		--landlock-rule path-beneath:read-file:/dev/null
+		--landlock-rule path-beneath:write-file:/dev/null
+
+		# Try to let pkgcheck emit errors when run under cron
+		--landlock-rule path-beneath:read-file:/dev/stdout
+		--landlock-rule path-beneath:read-file:/dev/stderr
+		--landlock-rule path-beneath:write-file:/dev/stdout
+		--landlock-rule path-beneath:write-file:/dev/stderr
+		--landlock-rule path-beneath:ioctl-dev:/dev/stdout
+		--landlock-rule path-beneath:ioctl-dev:/dev/stderr
+		--landlock-rule path-beneath:ioctl-dev:/dev/tty
+		--landlock-rule path-beneath:read-file:/dev/pts
+		--landlock-rule path-beneath:read-dir:/dev/pts
+		--landlock-rule path-beneath:write-file:/dev/pts
+		--landlock-rule path-beneath:ioctl-dev:/dev/pts
+
+		--landlock-rule path-beneath:read-file:/dev/tty
+		--landlock-rule path-beneath:write-file:/dev/tty
+
+		--landlock-rule path-beneath:write-file:/tmp
+
+		# sandbox.log
+		--landlock-rule path-beneath:make-reg:/tmp
+		--landlock-rule path-beneath:remove-file:/tmp
+
+		--landlock-rule path-beneath:read-dir:/etc/sandbox.d
+		--landlock-rule path-beneath:read-file:/etc/sandbox.d
+		--landlock-rule path-beneath:read-file:/etc/sandbox.conf
+		--landlock-rule path-beneath:read-file:/usr/share/sandbox/sandbox.bashrc
+
+		# Needed for make.profile symlink
+		--landlock-rule path-beneath:read-dir:/var/db/repos/gentoo
+		--landlock-rule path-beneath:read-file:/var/db/repos/gentoo
+
+		--landlock-rule path-beneath:read-dir:\${portage_dir}
+		--landlock-rule path-beneath:read-file:\${portage_dir}
+
+		# Any repository may need to read any other repository because
+		# of repo masters.
+		--landlock-rule path-beneath:read-dir:\${repos_dir}
+		--landlock-rule path-beneath:read-file:\${repos_dir}
+
+		# Python's multiprocessing module creates locks here
+		--landlock-rule path-beneath:read-dir:/dev/shm
+		--landlock-rule path-beneath:read-file:/dev/shm
+		--landlock-rule path-beneath:write-file:/dev/shm
+		--landlock-rule path-beneath:make-reg:/dev/shm
+		--landlock-rule path-beneath:remove-file:/dev/shm
+
+		--landlock-rule path-beneath:read-dir:${WORKER_DIR}/.cache/pkgcheck
+		--landlock-rule path-beneath:read-file:${WORKER_DIR}/.cache/pkgcheck
+		--landlock-rule path-beneath:write-file:${WORKER_DIR}/.cache/pkgcheck
+		--landlock-rule path-beneath:remove-dir:${WORKER_DIR}/.cache/pkgcheck
+		--landlock-rule path-beneath:remove-file:${WORKER_DIR}/.cache/pkgcheck
+		--landlock-rule path-beneath:truncate:${WORKER_DIR}/.cache/pkgcheck
+		--landlock-rule path-beneath:make-dir:${WORKER_DIR}/.cache/pkgcheck
+
+		# Used to compress cache
+		--landlock-rule path-beneath:execute:/usr/bin/zstd
+		--landlock-rule path-beneath:read-file:/usr/bin/zstd
+
+		# PerlCheck
+		--landlock-rule path-beneath:execute:/usr/bin/perl
+		--landlock-rule path-beneath:read-file:/usr/bin/perl
+		--landlock-rule path-beneath:read-file:/usr/share/pkgcheck/perl-version.pl
+		--landlock-rule path-beneath:read-dir:/usr/lib64/perl5
+		--landlock-rule path-beneath:read-file:/usr/lib64/perl5
+	)
+
+	# pkgcheck queries git for caching purposes
+	for bin in /usr/bin/git /usr/libexec/git-core ; do
+		setpriv_args+=(
+			--landlock-rule path-beneath:read-file:\${bin}
+			--landlock-rule path-beneath:execute:\${bin}
+		)
+	done
+
+	# Needed for Python to be able to find libb2 for hashlib
+	for file in /etc/ld.so.cache ; do
+		setpriv_args+=(
+			--landlock-rule path-beneath:read-file:\${file}
+		)
+	done
+	# Needed to call pkgcheck or called by pkgcheck
+	for file in /usr/bin/pkgcheck /usr/bin/python3.?? /usr/bin/python-exec2c /bin/bash \
+			/usr/bin/sandbox /bin/stty /usr/bin/env \
+			/usr/bin/readlink /usr/bin/sort ; do
+		setpriv_args+=(
+			--landlock-rule path-beneath:read-file:\${file}
+			--landlock-rule path-beneath:execute:\${file}
+		)
+	done
+	for dir in /usr/lib/python-exec /usr/lib64/python-exec ; do
+		setpriv_args+=(
+			--landlock-rule path-beneath:read-dir:\${dir}
+			--landlock-rule path-beneath:read-file:\${dir}
+			--landlock-rule path-beneath:execute:\${dir}
+		)
+	done
+	# Not just for Python itself but also the loader..
+	for dir in /usr/lib64 /lib64 /usr/lib/pkgcore ; do
+		setpriv_args+=(
+			--landlock-rule path-beneath:read-dir:\${dir}
+			--landlock-rule path-beneath:read-file:\${dir}
+			--landlock-rule path-beneath:execute:\${dir}
+		)
+	done
+	# site-packages
+	for dir in /usr/lib/python3.?? /etc/python-exec ; do
+		setpriv_args+=(
+			--landlock-rule path-beneath:read-dir:\${dir}
+			--landlock-rule path-beneath:read-file:\${dir}
+		)
+	done
+	# portage+pkgcore config
+	for dir in /usr/share/portage /usr/share/pkgcore ; do
+		setpriv_args+=(
+			--landlock-rule path-beneath:read-dir:\${dir}
+			--landlock-rule path-beneath:read-file:\${dir}
+		)
+	done
+	exec setpriv "\${setpriv_args[@]}" -- "\$@"
+	EOF
+
+	chmod +x /var/lib/repo-mirror-ci/pkgcheck-wrapper
+}
+
 cd -- "${SYNC_DIR}"/gentoo
 touch -r "${MIRROR_DIR}"/gentoo/metadata/timestamp.chk .git/timestamp
 CURRENT_COMMIT=$(git rev-parse --short HEAD)
@@ -27,12 +171,18 @@ if [[ ${PREV_COMMIT} != ${CURRENT_COMMIT} ]]; then
 
 	export CONFIG_DIR=${CONFIG_ROOT_GENTOO_CI}/etc/portage
 
-	# TODO: setpriv
+	create_pkgcheck_setpriv_wrapper
+
 	cd -- "${MIRROR_DIR}"/gentoo
 	sudo -u "${WORKER_USER}" \
 		bwrap --bind / / --dev /dev --proc /proc --unshare-all \
 		--uid $(id -u "${WORKER_USER}") --gid $(id -g "${WORKER_USER}") \
-		time timeout -k 30s "${CI_TIMEOUT}" pkgcheck --config "${CONFIG_DIR}" scan \
+		time timeout -k 30s "${CI_TIMEOUT}" \
+		/var/lib/repo-mirror-ci/pkgcheck-wrapper \
+		"${CONFIG_ROOT_GENTOO_CI}/etc/portage" \
+		"${MIRROR_DIR}" \
+		"${MIRROR_DIR}/${name}" \
+		pkgcheck --config "${CONFIG_DIR}" scan \
 		--reporter XmlReporter ${PKGCHECK_OPTIONS} > output.xml.tmp
 	# Sort XML for better Git delta compression
 	cat output.xml.tmp | xsltproc "${SCRIPT_DIR}"/sort-output.xsl - > output.xml
