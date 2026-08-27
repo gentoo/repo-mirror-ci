gentoo/repo-mirror-ci

commit 01922e5c129cb357729ac4d5f64ecd2e47194544

Allow libgcc_s to be loaded
For example, glibc -> pthread_cancel needs this.

Reported-by: Arthur Zamarin <arthurzam@gentoo.org>
Signed-off-by: Sam James <sam@gentoo.org>

pull-request/pull-requests-worker.bash
@@ -126,7 +126,7 @@ create_pmaint_setpriv_wrapper() {
 		)
 	done
 	# Not just for Python itself but also the loader..
-	for dir in /usr/lib64 /lib64 /usr/lib/pkgcore ; do
+	for dir in /usr/lib/gcc /usr/lib64 /lib64 /usr/lib/pkgcore ; do
 		setpriv_args+=(
 			--landlock-rule path-beneath:read-dir:\${dir}
 			--landlock-rule path-beneath:read-file:\${dir}

gentoo-ci/gentoo-ci.bash
@@ -133,7 +133,7 @@ create_pkgcheck_setpriv_wrapper() {
 		)
 	done
 	# Not just for Python itself but also the loader..
-	for dir in /usr/lib64 /lib64 /usr/lib/pkgcore ; do
+	for dir in /usr/lib/gcc /usr/lib64 /lib64 /usr/lib/pkgcore ; do
 		setpriv_args+=(
 			--landlock-rule path-beneath:read-dir:\${dir}
 			--landlock-rule path-beneath:read-file:\${dir}

repos/repos.bash
@@ -100,7 +100,7 @@ create_base_setpriv_wrapper() {
 		)
 	done
 	# Not just for Python itself but also the loader..
-	for dir in /usr/lib64 /lib64 /usr/lib/pkgcore ; do
+	for dir in /usr/lib/gcc /usr/lib64 /lib64 /usr/lib/pkgcore ; do
 		setpriv_args+=(
 			--landlock-rule path-beneath:read-dir:\${dir}
 			--landlock-rule path-beneath:read-file:\${dir}

commit aecb49f686b9eb8feaa74b3e19d1eadfa44f2a2b

pull-requests-worker.bash: allow /usr/lib/gcc in another place
Fixes: 01922e5c129cb357729ac4d5f64ecd2e47194544
Signed-off-by: Sam James <sam@gentoo.org>

pull-request/pull-requests-worker.bash
@@ -273,7 +273,7 @@ create_pkgcheck_setpriv_wrapper() {
 		)
 	done
 	# Not just for Python itself but also the loader..
-	for dir in /usr/lib64 /lib64 /usr/lib/pkgcore ; do
+	for dir in /usr/lib/gcc /usr/lib64 /lib64 /usr/lib/pkgcore ; do
 		setpriv_args+=(
 			--landlock-rule path-beneath:read-dir:\${dir}
 			--landlock-rule path-beneath:read-file:\${dir}