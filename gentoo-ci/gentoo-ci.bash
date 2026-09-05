gentoo/repo-mirror-ci

commit 0bde2aff3e60e5027e0099c6a4d971caff60b7cc

Allow /usr/bin/ld and friends for Python's find_library
pkgcore -> py-landlock uses Python's ctypes.utils.find_library to find
libc, and apparently find_library invokes ld(1).

Signed-off-by: Sam James <sam@gentoo.org>

gentoo-ci/gentoo-ci.bash
@@ -145,6 +145,14 @@ create_pkgcheck_setpriv_wrapper() {
 			--landlock-rule path-beneath:execute:\${dir}
 		)
 	done
+	# Python's ctypes.util.find_library invokes ld(1)!
+	for dir in /usr/bin/ld /usr/$(portageq envvar CHOST)/bin ; do
+		setpriv_args+=(
+			--landlock-rule path-beneath:read-dir:\${dir}
+			--landlock-rule path-beneath:read-file:\${dir}
+			--landlock-rule path-beneath:execute:\${dir}
+		)
+	done
 	# site-packages
 	for dir in /usr/lib/python3.?? /etc/python-exec ; do
 		setpriv_args+=(

pull-request/pull-requests-worker.bash
@@ -138,6 +138,14 @@ create_pmaint_setpriv_wrapper() {
 			--landlock-rule path-beneath:execute:\${dir}
 		)
 	done
+	# Python's ctypes.util.find_library invokes ld(1)!
+	for dir in /usr/bin/ld /usr/$(portageq envvar CHOST)/bin ; do
+		setpriv_args+=(
+			--landlock-rule path-beneath:read-dir:\${dir}
+			--landlock-rule path-beneath:read-file:\${dir}
+			--landlock-rule path-beneath:execute:\${dir}
+		)
+	done
 	# site-packages
 	for dir in /usr/lib/python3.?? /etc/python-exec ; do
 		setpriv_args+=(
@@ -284,6 +292,14 @@ create_pkgcheck_setpriv_wrapper() {
 	done
 	# Not just for Python itself but also the loader..
 	for dir in /usr/lib/gcc /usr/lib64 /lib64 /usr/lib/pkgcore ; do
+		setpriv_args+=(
+			--landlock-rule path-beneath:read-dir:\${dir}
+			--landlock-rule path-beneath:read-file:\${dir}
+			--landlock-rule path-beneath:execute:\${dir}
+		)
+	done
+	# Python's ctypes.util.find_library invokes ld(1)!
+	for dir in /usr/bin/ld /usr/$(portageq envvar CHOST)/bin ; do
 		setpriv_args+=(
 			--landlock-rule path-beneath:read-dir:\${dir}
 			--landlock-rule path-beneath:read-file:\${dir}

repos/repos.bash
@@ -112,6 +112,14 @@ create_base_setpriv_wrapper() {
 			--landlock-rule path-beneath:execute:\${dir}
 		)
 	done
+	# Python's ctypes.util.find_library invokes ld(1)!
+	for dir in /usr/bin/ld /usr/$(portageq envvar CHOST)/bin ; do
+		setpriv_args+=(
+			--landlock-rule path-beneath:read-dir:\${dir}
+			--landlock-rule path-beneath:read-file:\${dir}
+			--landlock-rule path-beneath:execute:\${dir}
+		)
+	done
 	# site-packages
 	for dir in /usr/lib/python3.?? /etc/python-exec ; do
 		setpriv_args+=(

commit c32fe9b4a44e9cc2efc6f02b18febfbb9749bb3f

Split /usr/bin/ld rule out
Fixes: 0bde2aff3e60e5027e0099c6a4d971caff60b7cc
Signed-off-by: Sam James <sam@gentoo.org>

gentoo-ci/gentoo-ci.bash
@@ -146,7 +146,13 @@ create_pkgcheck_setpriv_wrapper() {
 		)
 	done
 	# Python's ctypes.util.find_library invokes ld(1)!
-	for dir in /usr/bin/ld /usr/$(portageq envvar CHOST)/bin ; do
+	for file in /usr/bin/ld ; do
+		setpriv_args+=(
+			--landlock-rule path-beneath:read-file:\${file}
+			--landlock-rule path-beneath:execute:\${file}
+		)
+	done
+	for dir in /usr/$(portageq envvar CHOST)/bin ; do
 		setpriv_args+=(
 			--landlock-rule path-beneath:read-dir:\${dir}
 			--landlock-rule path-beneath:read-file:\${dir}

pull-request/pull-requests-worker.bash
@@ -139,7 +139,13 @@ create_pmaint_setpriv_wrapper() {
 		)
 	done
 	# Python's ctypes.util.find_library invokes ld(1)!
-	for dir in /usr/bin/ld /usr/$(portageq envvar CHOST)/bin ; do
+	for file in /usr/bin/ld ; do
+		setpriv_args+=(
+			--landlock-rule path-beneath:read-file:\${file}
+			--landlock-rule path-beneath:execute:\${file}
+		)
+	done
+	for dir in /usr/$(portageq envvar CHOST)/bin ; do
 		setpriv_args+=(
 			--landlock-rule path-beneath:read-dir:\${dir}
 			--landlock-rule path-beneath:read-file:\${dir}
@@ -299,7 +305,13 @@ create_pkgcheck_setpriv_wrapper() {
 		)
 	done
 	# Python's ctypes.util.find_library invokes ld(1)!
-	for dir in /usr/bin/ld /usr/$(portageq envvar CHOST)/bin ; do
+	for file in /usr/bin/ld ; do
+		setpriv_args+=(
+			--landlock-rule path-beneath:read-file:\${file}
+			--landlock-rule path-beneath:execute:\${file}
+		)
+	done
+	for dir in /usr/$(portageq envvar CHOST)/bin ; do
 		setpriv_args+=(
 			--landlock-rule path-beneath:read-dir:\${dir}
 			--landlock-rule path-beneath:read-file:\${dir}

repos/repos.bash
@@ -113,7 +113,13 @@ create_base_setpriv_wrapper() {
 		)
 	done
 	# Python's ctypes.util.find_library invokes ld(1)!
-	for dir in /usr/bin/ld /usr/$(portageq envvar CHOST)/bin ; do
+	for file in /usr/bin/ld ; do
+		setpriv_args+=(
+			--landlock-rule path-beneath:read-file:\${file}
+			--landlock-rule path-beneath:execute:\${file}
+		)
+	done
+	for dir in /usr/$(portageq envvar CHOST)/bin ; do
 		setpriv_args+=(
 			--landlock-rule path-beneath:read-dir:\${dir}
 			--landlock-rule path-beneath:read-file:\${dir}